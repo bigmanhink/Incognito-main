897594b414d4d5d6bc259769d0f78914ed72b4a2
bigmanhink <118319320+bigmanhink@users.noreply.github.com>
2022-11-25 03:33:32 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,20 @@
 self.__uv$config = {
+    port: 8080,
+    ssl: false,
     prefix: '/service/',
+    codec: "xor",
     bare: 'https://incog.dev/bare/',
+    proxy: true,
+    authorization: {
+        "name": "__incog_auth",
+        "value": "1"
+    },
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
     bundle: '/uv.bundle.js',
     config: '/uv.config.js',
     sw: '/uv.sw.js',
+    appearance: "bright",
+    engine: "google"
 };
