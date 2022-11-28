ce5986f2e7e1da29091aea50b7c7786e8c2fdac4
bigmanhink <118319320+bigmanhink@users.noreply.github.com>
2022-11-28 02:47:38 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 import EventEmitter from "./events.js";
-import { createComponent } from './component.js';
-
+import {
+    createComponent
+} from './component.js';
 class App extends EventEmitter {
     constructor(hashes = []) {
         super();
@@ -27,29 +28,24 @@ class App extends EventEmitter {
     };
     createElement(elemName, content = '', config = {}) {
         const element = document.createElement(elemName);
-    
         if ('events' in config) {
             for (const name in config.events) {
                 element.addEventListener(name, config.events[name]);
             };
         };
-
         if ('attrs' in config) {
             for (const name in config.attrs) {
                 element.setAttribute(name, config.attrs[name])
             };
         };
-    
         if ('style' in config && 'style' in element) {
             for (const name in config.style) {
                 element.style.setProperty(name, config.style[name])
             };
         };
-    
         if ('id' in config) {
             element.id = config.id;
         };
-    
         if ('class' in config) {
             if (Array.isArray(config.class)) {
                 element.classList.add(...config.class);
@@ -57,25 +53,19 @@ class App extends EventEmitter {
                 element.className = config.class;
             };
         };
-    
         if (typeof content === 'object') {
             if (Array.isArray(content)) {
-                element.append(
-                    ...content.filter(node => !!node)
-                );
+                element.append(...content.filter(node => !!node));
             } else {
                 element.append(content);
             }
         } else {
             element.innerHTML = content;
         };
-    
         if ('decorate' in config) {
             config.decorate(element);
         };
-
         return element;
     };
 };
-
 export { App };
