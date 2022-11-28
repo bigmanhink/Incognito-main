8dffb5465303e9c86415776ed24920f257caba81
adminyt4 <jaxsenhinkle@yahoo.com>
2022-11-27 20:53:26 -0500

    fixed

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,3 @@
-const split=location.hostname.split('.')
-const domain=split.slice(0).slice(-(split.length===4?3:2)).join('.')
 self.__uv$config={
     prefix: '/service/',
     bare: 'https://client.incog.dev',
